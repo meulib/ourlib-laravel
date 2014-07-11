7b052fd5d1a2aaea7a6bd7ae5057725d41edde87
Vani Murarka <vani.murarka@gmail.com>
2014-07-11 20:27:07 +0530

    link to single book page

--- app/views/booksIndex.blade.php
+++ app/views/booksIndex.blade.php
@@ -1,20 +1,23 @@
+
 @extends('templates.base')
 
 <ul>
 @section('content')
 	@foreach($books as $book)
 		{{-- var_dump($book) --}}
-		<li>{{{ $book->Title }}}
-		@if ($book->SubTitle)
-			{{{ ": ".$book->SubTitle }}}
-		@endif
-		@if ($book->Author1)
-			{{{ "&nbsp;by ".$book->Author1 }}}
-		@endif
-		@if ($book->Author2)
-			{{{ ", ".$book->Author2 }}}
-		@endif
-		<br/>
+		<li>
+			<a href={{  URL::action('BookController@showSingle', array($book->ID))}}>
+			{{{ $book->Title }}}
+			@if ($book->SubTitle)
+				{{{ ": ".$book->SubTitle }}}
+			@endif
+			</a>
+			@if ($book->Author1)
+				{{{ "&nbsp;by ".$book->Author1 }}}
+			@endif
+			@if ($book->Author2)
+				{{{ ", ".$book->Author2 }}}
+			@endif	
 	@endforeach
 @stop
 </ul>
