4f1b5e01c31a77a726a0d18e4f5d38193cbea5e0
Vani Murarka <vani.murarka@gmail.com>
2014-07-28 18:33:18 +0530

    request5

--- app/controllers/TransactionController.php
+++ app/controllers/TransactionController.php
@@ -11,8 +11,21 @@ class TransactionController extends BaseController
         $userID = Session::get('loggedInUser')->UserID;
         $bookCopyID = Input::get('bookCopyID');
         $msg = Input::get('requestMessage');
-        $tranID = Transaction::request($userID,$bookCopyID,$msg);
-        echo $tranID;
+        try
+        {
+            $tranID = Transaction::request($userID,$bookCopyID,$msg);
+        }
+        catch (Exception $e)
+        {
+            Session::put('TransactionMessage',['RequestBook','There was some error. Request not sent.']);
+        }        
+
+        if ($tranID > 0)
+            Session::put('TransactionMessage',['RequestBook','Request Sent successfully']);
+        else
+           Session::put('TransactionMessage',['RequestBook','There was some error. Request not sent.']);
+
+        return Redirect::to(URL::previous());
     }
 }
 ?>
